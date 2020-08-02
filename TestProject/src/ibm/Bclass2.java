1286da3dacd8bb819b8a8464ce35e3a2939ec46f
Samuel Karabaš <samuel@cassoviacode.com>
2020-08-02 11:02:14 +0200

    first commit

---
+++ TestProject/src/ibm/Bclass2.java
@@ -0,0 +1,39 @@
+package ibm;
+
+public class Bclass2 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("aa");
+		
+		int a= 5;
+		int b = 3;
+		
+		if(a > 5) {
+			System.out.print("aa");	
+		}
+		
+		if(a < 5) {
+			System.out.print("bb");	
+		}
+		
+		if(a==5) {
+			
+			if(b==3) {
+				a+=b;
+			}
+			
+			System.out.print("cc");		
+		}
+		
+
+	}
+	
+	
+	public void name() {
+
+		
+		
+	}
+
+}
